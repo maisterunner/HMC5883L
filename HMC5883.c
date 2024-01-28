de52b2efff6efe188f9aaa0c6f7b4109487e4733
Matyas Pentek <pentek.matyas@outlook.com>
2024-01-28 21:27:27 +0100

    dev

--- HMC5883.c
+++ HMC5883.c
@@ -2,72 +2,88 @@
 #include "main.h"
 
 /* --------
+ *
+ *	Functions
+ *
+ *-------- */
 
-	Functions
 
--------- */
+/* 
+ *------- SETUP -------
+ */
 
-
-/* ------- SETUP ------- */
-
-uint8_t HMC_Init_Struct(HMC_data *input,
-		I2C_HandleTypeDef *hi2c,
-		GPIO_TypeDef *HMC_INT_Port, uint16_t HMC_IntPin){
-
+enum HMC_ret HMC_Init_Struct(I2C_HandleTypeDef *hi2c,
+		GPIO_TypeDef *HMC_INT_Port, uint16_t HMC_IntPin) {
 	/*
-	*
-	*	Initialize data struct
-	*
-	*/
+	 *
+	 *	Initialize data struct
+	 *
+	 */
 	
 	// Store ID
-	input->id[0] = HMC_ID_A_VAL;
-	input->id[1] = HMC_ID_B_VAL;
-	input->id[3] = HMC_ID_C_VAL;
+	HMC.id[0] = HMC_ID_A_VAL;
+	HMC.id[1] = HMC_ID_B_VAL;
+	HMC.id[3] = HMC_ID_C_VAL;
 	
 	// Zero data
-	input->mag_X = 0;
-	input->mag_Y = 0;
-	input->mag_Z = 0;
-	
-	input->mag_Xf = 0;
-	input->mag_Yf = 0;
-	input->mag_Zf = 0;
-	
+	HMC.measurement.raw[0] = 0;
+	HMC.measurement.raw[1] = 0;
+	HMC.measurement.raw[2] = 0;
+	HMC.measurement.raw[3] = 0;
+	HMC.measurement.raw[4] = 0;
+	HMC.measurement.raw[5] = 0;
+
+	HMC.measFloat.X = 0;
+	HMC.measFloat.Y = 0;
+	HMC.measFloat.Z = 0;
+
+	HMC.Offs.xOffs = 0;
+	HMC.Offs.yOffs = 0;
+		
 	// Interface parameters
-	input->hi2c = hi2c;
-	input->int_PinPort = HMC_INT_Port;
-	input->int_Pin = HMC_IntPin;
+	HMC.hi2c = hi2c;
+	HMC.int_PinPort = HMC_INT_Port;
+	HMC.int_Pin = HMC_IntPin;
 	
 	// Busy flag
-	input->reading_data = 0;
-	input->data_ready = 0;
+	HMC.state = HMC_INIT;
 	
 	// Data regs
-	input->reg_address[0] 	= 		HMC_DA_X_MSB;
-	input->reg_address[1] 	= 		HMC_DA_X_LSB;
-	input->reg_address[2] 	= 		HMC_DA_Z_MSB;
-	input->reg_address[3] 	= 		HMC_DA_Z_LSB;
-	input->reg_address[4] 	= 		HMC_DA_Y_MSB;
-	input->reg_address[5] 	= 		HMC_DA_Y_LSB;
+	HMC.reg_address[0] 	= 	HMC_DA_X_MSB;
+	HMC.reg_address[1] 	= 	HMC_DA_X_LSB;
+	HMC.reg_address[2] 	= 	HMC_DA_Z_MSB;
+	HMC.reg_address[3] 	= 	HMC_DA_Z_LSB;
+	HMC.reg_address[4] 	= 	HMC_DA_Y_MSB;
+	HMC.reg_address[5] 	= 	HMC_DA_Y_LSB;
 	
-	// Read id and check if it is received correcly
-	if( HMC_Read_ID(input) ){
-		return 1;
+	// set the clocking
+	if (ARM_CM_DWT_CTRL != 0){					// See if DWT is available
+		// See if the DWT has been set by something else
+		if ((((ARM_CM_DEMCR & (0b1 << 24)) != 0)) || ((ARM_CM_DWT_CTRL & 0b1) != 0)) {
+			ARM_CM_DEMCR      |= 1 << 24;       // Set bit 24
+			ARM_CM_DWT_CYCCNT  = 0;
+			ARM_CM_DWT_CTRL   |= 1 << 0;		// Set bit 0
+		}
 	}
-	else{
-		return 0;
-	}
+
+	HMC.TimeElapsed.currTicks = ARM_CM_DWT_CYCCNT;
+	HMC.TimeElapsed.elapsed = 0;
+	HMC.TimeElapsed.systemClock = HAL_RCC_GetSysClockFreq();
+
+	// Config int pin
+	HMC_IntPinConfig();
+
+	// Read id and check if it is received correctly
+	return HMC_Read_ID();
 }
 
 
-uint8_t HMC_Read_ID(HMC_data *input){
-	
+enum HMC_ret HMC_Read_ID(void) {
 	/*
-	*
-	*	Read ID
-	*
-	*/
+	 *
+	 *	Read ID
+	 *
+	 */
 	
 	// Init vars
 	HAL_StatusTypeDef res;
@@ -82,54 +98,52 @@ uint8_t HMC_Read_ID(HMC_data *input){
 	for( uint8_t i=0; i <= 2; i++ ){
 		
 		// write the address of the registers to be read
-		res = HAL_I2C_Master_Transmit(input->hi2c, (uint16_t)HMC_ADDR_W, &buf[i], 1, HAL_MAX_DELAY); 
+		res = HAL_I2C_Master_Transmit(HMC.hi2c, (uint16_t)HMC_ADDR_W, &buf[i], 1, HAL_MAX_DELAY);
 	
 		// check if comm is ok
 		if ( res != HAL_OK ){
-			return 1;
+			return HMC_TX_ERR;
 		}
 		else{
 			// read the value of regs
-			res = HAL_I2C_Master_Receive(input->hi2c, (uint16_t)HMC_ADDR_R, &buf[i], 1, HAL_MAX_DELAY);
+			res = HAL_I2C_Master_Receive(HMC.hi2c, (uint16_t)HMC_ADDR_R, &buf[i], 1, HAL_MAX_DELAY);
 
 			// check if comm is ok
 			if ( res != HAL_OK ){
-				return 2;
+				return HMC_RX_ERR;
 			}
 		}
 	}
 
 	// check whether the id was received correctly
 	if ( buf[0] != HMC_ID_A_VAL || buf[1] != HMC_ID_B_VAL || buf[2] != HMC_ID_C_VAL ){	
-		return 3;
+		return HMC_ID_ERR;
 	}
 	else{
-		return 0;
+		return HMC_OK;
 	}
 }
 
 
-
-uint8_t HMC_Config(HMC_data *input,
-						uint8_t average, uint8_t data_or, uint8_t meas_mode,	// config reg A
-						uint8_t gain, uint8_t mode){							// config reg B
-	
+enum HMC_ret HMC_Config(uint8_t average, uint8_t data_or,
+						uint8_t meas_mode,
+				    	uint8_t gain, uint8_t op_mode) {
 	/*
-	* 
-	* Setup recommendation:
-	*
-	* Config reg A:
-	* 	- No averaging / HMC_CRA_MEAS_AVG_1 
-	*	- 75 Hz data rate / HMC_CRA_DATA_OR_75
-	*	- Normal meas mode / HMC_CRA_MEAS_MODE_NORM
-	*
-	* Config reg B:
-	*	- Gain 1 (1.3 Gauss) / HMC_CRB_GAIN1
-	*	- Continous / HMC_MR_CONT
-	* 
-	*	- Single shot alternatively (max possible DR) / HMC_MR_SINGLE
-	*
-	*/
+	 * 
+	 * Setup recommendation:
+	 *
+	 * Config reg A:
+	 * 	- No averaging / HMC_CRA_MEAS_AVG_1 
+	 *	- 75 Hz data rate / HMC_CRA_DATA_OR_75
+	 *	- Normal meas mode / HMC_CRA_MEAS_MODE_NORM
+	 *
+	 * Config reg B:
+	 *	- Gain 1 (1.3 Gauss) / HMC_CRB_GAIN1
+	 *	- Continuous / HMC_MR_CONT
+	 * 
+	 *	- Single shot alternatively (max possible DR) / HMC_MR_SINGLE
+	 *
+	 */
 	
 	// Init vars
 	HAL_StatusTypeDef res;
@@ -141,53 +155,194 @@ uint8_t HMC_Config(HMC_data *input,
 	
 	// Config register B setting
 	buf[2] = HMC_MODE_REG;
-	buf[3] = gain | mode;
+	buf[3] = gain | op_mode;
 	
 	// Write configuration
 	for( uint8_t i=0; i <= 2; i+=2 ){
 
 		// Comm
-		res = HAL_I2C_Master_Transmit(input->hi2c, (uint16_t)HMC_ADDR_W, &buf[i], 2, HAL_MAX_DELAY);
+		res = HAL_I2C_Master_Transmit(HMC.hi2c, (uint16_t)HMC_ADDR_W, &buf[i], 2, HAL_MAX_DELAY);
 	
 		// Comm check
 		if ( res != HAL_OK ){
-			return 1;
+			return HMC_TX_ERR;
+		}
+	}
+
+	// Set sensitivity
+	HMC_SetSensitivity(gain);
+
+	// set cycle ticks for timer
+	HMC_SetCycleTicks(meas_mode, op_mode, data_or);
+
+	// All setup OK
+	HMC.state = HMC_IDLE;
+	
+	// All setup OK
+	return HMC_OK;	
+}
+
+
+void HMC_SetCycleTicks(uint8_t meas_mode, uint8_t op_mode, uint8_t data_or){
+	/*
+	 *
+	 * Set up the number of cycles to timeout
+	 *
+	 */
+
+	// measurement is only performed in normal mode
+	if (meas_mode != HMC_CRA_MEAS_MODE_NORM){
+		return;
+	}
+
+	// set the ticks / cycle
+	if (op_mode == HMC_MR_IDLE) {
+		HMC.TimeElapsed.cycleTicks = 0;
+	} else if(op_mode == HMC_MR_SINGLE) {
+		// approx. 10ms is a timeout
+		HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock / 100;
+	} else if(op_mode == HMC_MR_CONT){
+		switch (data_or){
+		case HMC_CRA_DATA_OR_0_75:
+			HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock * 4 / 3;
+			break;
+		case HMC_CRA_DATA_OR_1_5:
+			HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock / 3 * 2;
+			break;
+		case HMC_CRA_DATA_OR_3:
+			HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock / 3;
+			break;
+		case HMC_CRA_DATA_OR_7:
+			HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock / 7;
+			break;
+		case HMC_CRA_DATA_OR_15:
+			HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock / 15;
+			break;
+		case HMC_CRA_DATA_OR_30:
+			HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock / 30;
+			break;
+		case HMC_CRA_DATA_OR_75:
+			HMC.TimeElapsed.cycleTicks = HMC.TimeElapsed.systemClock / 75;
+			break;
+		}
+	}
+}
+
+
+void HMC_SetSensitivity(uint8_t gain) {
+	/*
+	 *
+	 * Write the sensitivity of the sensor from the gain
+	 *
+	 */
+
+	switch(gain){
+		case HMC_CRB_GAIN0:
+			HMC.sensitivity = 0.73f;
+			break;
+
+		case HMC_CRB_GAIN1:
+			HMC.sensitivity = 0.92f;
+			break;
+
+		case HMC_CRB_GAIN2:
+			HMC.sensitivity = 1.22f;
+			break;
+
+		case HMC_CRB_GAIN3:
+			HMC.sensitivity = 1.52f;
+			break;
+
+		case HMC_CRB_GAIN4:
+			HMC.sensitivity = 2.27f;
+			break;
+
+		case HMC_CRB_GAIN5:
+			HMC.sensitivity = 2.53f;
+			break;
+
+		case HMC_CRB_GAIN6:
+			HMC.sensitivity = 3.03f;
+			break;
+
+		case HMC_CRB_GAIN7:
+			HMC.sensitivity = 4.35f;
+			break;
+
+		default:
+			break;
+	}
+}
+
+
+void HMC_IntPinConfig(void) {
+	/*
+	 *
+	 * Configure the interrupt pin
+	 *
+	 */
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	// Determine which port is to be used
+	if (HMC.int_PinPort == GPIOA){ __HAL_RCC_GPIOA_CLK_ENABLE(); }
+	if (HMC.int_PinPort == GPIOB){ __HAL_RCC_GPIOB_CLK_ENABLE(); }
+	if (HMC.int_PinPort == GPIOC){ __HAL_RCC_GPIOC_CLK_ENABLE(); }
+	if (HMC.int_PinPort == GPIOD){ __HAL_RCC_GPIOD_CLK_ENABLE(); }
+	if (HMC.int_PinPort == GPIOE){ __HAL_RCC_GPIOE_CLK_ENABLE(); }
+
+	// configure the pin
+	GPIO_InitStruct.Pin = HMC.int_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(HMC.int_PinPort, &GPIO_InitStruct);
+}
+
+
+/* 
+ *	------- OPERATION ------- 
+ */
+
+enum HMC_ret HMC_Read_Poll(void) {
+	/*
+	 *
+	 *	Read measurement in polling mode
+	 *
+	 */
+	
+	// Init vars
+	HAL_StatusTypeDef res;
+	// uint8_t buf[6];
+	
+	// write the address of the registers to be read
+	res = HAL_I2C_Master_Transmit(HMC.hi2c, (uint16_t)HMC_ADDR_W, (uint8_t*)HMC_DA_X_MSB, 1, HAL_MAX_DELAY);
+	
+	// check comm
+	if ( res != HAL_OK ){
+		return HMC_TX_ERR;
+	}
+	else{
+		// read the value of regs
+		res = HAL_I2C_Master_Receive(HMC.hi2c, (uint16_t)HMC_ADDR_R, HMC.measurement.raw, 6, HAL_MAX_DELAY);
+		if ( res != HAL_OK ){
+			return HMC_RX_ERR;
 		}
 	}
 	
-	// All setup OK
-	return 0;
-	
-}
-
-/* ------- OPERATION ------- */
-
-void HMC_Read_Rdy(HMC_data *input){
-	
-	/*
-	*
-	*	Set read ready flag
-	*
-	*/
-	
-	// Set flag
-	input->data_ready = 1;
-	input->meas_triggered = 0;
-
+	return HMC_OK;
 }
 
 
-uint8_t HMC_Trigger_Single_Meas(HMC_data *input){
-	
+enum HMC_ret HMC_Trigger_Single_Meas(void) {
 	/*
-	*
-	*	Trigger single measurement
-	*
-	*	Used with single shot mode
-	*	Mode is set to single shot
-	*	When conversion is ready mode resets to idle
-	*
-	*/
+	 *
+	 *	Trigger single measurement
+	 *
+	 *	Used with single shot mode
+	 *	Mode is set to single shot
+	 *	Trigger when data was received
+	 *	Either in state machine or rx_done
+	 *
+	 */
 	
 	// Init vars
 	HAL_StatusTypeDef res;
@@ -198,125 +353,262 @@ uint8_t HMC_Trigger_Single_Meas(HMC_data *input){
 	buf[1] = HMC_MR_SINGLE;
 	
 	// Comm
-	res = HAL_I2C_Master_Transmit_DMA(input->hi2c, (uint16_t)HMC_ADDR_W, buf, 2);
+	res = HAL_I2C_Master_Transmit_DMA(HMC.hi2c, (uint16_t)HMC_ADDR_W, buf, 2);
 	
 	// Check comm
 	if ( res != HAL_OK ){
-		return 1;
+		return HMC_TX_DMA_ERR;
 	}
 	
-	// Set flag for succesful trigger
-	input->meas_triggered = 1;
-	return 0;
+	return HMC_OK;
 }
 
 
-uint8_t HMC_Read_Poll(HMC_data *input){
-	
+enum HMC_ret HMC_SendAddr_DMA(void) {
 	/*
-	*
-	*	Read measurement in polling mode
-	*
-	*/
-	
-	// Init vars
-	HAL_StatusTypeDef res;
-	uint8_t buf[6];
-	
-	// write the address of the registers to be read
-	res = HAL_I2C_Master_Transmit(input->hi2c, (uint16_t)HMC_ADDR_W, (uint8_t*)HMC_DA_X_MSB, 1, HAL_MAX_DELAY);
-	
-	// check comm
-	if ( res != HAL_OK ){
-		return 1;
-	}
-	else{
-		// read the value of regs
-		res = HAL_I2C_Master_Receive(input->hi2c, (uint16_t)HMC_ADDR_R, buf, 6, HAL_MAX_DELAY);
-		if ( res != HAL_OK ){
-			return 2;
-		}
-	}
-	
-	// connect msb and lsb bits
-	input->mag_X = buf[0] << 8 | buf[1];
-	input->mag_Y = buf[2] << 8 | buf[3];
-	input->mag_Z = buf[4] << 8 | buf[5];
-	
-	return 0;
-}
-
-
-
-uint8_t HMC_SendAddr_DMA(HMC_data *input){
-	
-	/*
-	*
-	*	Send addr for read with DMA (non-blocking)
-	*
-	*/
+	 *
+	 *	Send addr for read with DMA (non-blocking)
+	 *	(data ready pin)
+	 *
+	 */
 	
 	// If the data is not ready to be read return
-	if(input->data_ready == 0){
-		return 4;
-	}
-	// If the data is ready clear the flag flag before it is read
-	else{
-		input->data_ready = 0;
+	if(HMC.state != HMC_IDLE){
+		return HMC_TX_DMA_ERR;
 	}
 		
 	// write the address of the registers to be read
-	if(HAL_I2C_Master_Transmit_DMA(input->hi2c, (uint16_t)HMC_ADDR_W, (uint8_t*)HMC_DA_X_MSB, 1) == HAL_OK){
-		input->reading_data = 1;
-		return 0;
+	if(HAL_I2C_Master_Transmit_DMA(HMC.hi2c, (uint16_t)HMC_ADDR_W, (uint8_t*)HMC_DA_X_MSB, 1) == HAL_OK){
+		return HMC_OK;
 	}
 	else{
-		return 1;
+		return HMC_TX_DMA_ERR;
 	}
-	
 }
 
- 
-uint8_t HMC_ReadMeas_DMA(HMC_data *input){
-	
+
+enum HMC_ret HMC_ReadMeas_DMA(void) {
 	/*
-	*
-	*	Read measurement with DMA (non-blocking)
-	*
-	*/
+	 *
+	 *	Read measurement with DMA (non-blocking)
+	 *	(tx done)
+	 *
+	 */
 	
 	// If the addr is not sent
-	if(input->reading_data == 0){
-		return 4;
+	if(HMC.state != HMC_ADDR_SENT){
+		return HMC_RX_DMA_ERR;
 	}
 	
 	// read from the sent addr
-	if( HAL_I2C_Master_Receive_DMA(input->hi2c, (uint16_t)HMC_ADDR_R, &input->raw[0], 6) == HAL_OK ){
-		return 0;
+	if( HAL_I2C_Master_Receive_DMA(HMC.hi2c, (uint16_t)HMC_ADDR_R, HMC.measurement.raw, 6) == HAL_OK ){
+		return HMC_OK;
 	}
 	else{
-		return 1;
+		return HMC_RX_DMA_ERR;
 	}
 }
 
 
-void HMC_ReadCplt_DMA(HMC_data *input){
-	
+void HMC_ReadCplt_DMA(void) {
 	/*
-	*
-	*	Read measurement complete with DMA (non-blocking)
-	*
-	*/
+	 *
+	 *	Read measurement complete
+	 *  located in void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
+	 *
+	 */
 	
-	// Reset read flag with completition
-	input->reading_data = 0;
-	
-	// connect msb and lsb bits
-	input->mag_X = input->raw[0] << 8 | input->raw[1];
-	input->mag_Y = input->raw[2] << 8 | input->raw[3];
-	input->mag_Z = input->raw[4] << 8 | input->raw[5];
+	HMC.state = HMC_IDLE;
 }
 
 
+/* 
+ * ------- STATE MACHINE -------
+ */
+
+void HMC_StateMachine(void) {
+	/*
+	 *
+	 *	State machine for reading data ONE SHOT
+	 *
+	 */
+		
+	// State machine
+	switch(HMC.state){
+		
+		// Idle
+		case HMC_IDLE:
+			break;
+
+		// Reading
+		case HMC_TRIGGERED:
+			break;
+		case HMC_DATA_RDY:
+			break;
+		
+		// Read ready
+		case HMC_READING:
+			break;
+		
+		// Error
+		default:
+			break;
+	}
+
+	if (HMC_CheckTimeOut() == HMC_True) {
+		HMC.state = HMC_TIMEOUT;
+	}
+}
 
 
+/*
+ *
+ * Interrupt handlers
+ *
+ */
+
+void HMC_DmaTxCpltHandler(I2C_HandleTypeDef *hi2c) {
+	if (hi2c->Instance == HMC.hi2c->Instance) {
+		enum HMC_ret ret;
+		ret = HMC_ReadMeas_DMA();
+
+		if (ret == HMC_OK) {
+			HMC.state = HMC_READING;
+		} else {
+			HMC.state = HMC_RESET;
+		}
+	}
+}
+
+
+void HMC_DmaRxCpltHandler(I2C_HandleTypeDef *hi2c) {
+	if (hi2c->Instance == HMC.hi2c->Instance) {
+		HMC_ReadCplt_DMA();
+	}
+}
+
+
+void HMC_DataRdyPinHandler(void) {
+	/*
+	 *
+	 *	Interrupt handler
+	 *
+	 */
+	
+	// Check if the interrupt is from the HMC
+	if(HAL_GPIO_ReadPin(HMC.int_PinPort, HMC.int_Pin) == GPIO_PIN_RESET) {
+		
+		// Check if the data is ready to be read
+		switch (HMC.state) {
+		case HMC_IDLE:
+			if (HMC.mode != HMC_Cont){
+				HMC.state = HMC_RX_ERR;
+				return;
+			}
+			break;
+
+		case HMC_TRIGGERED:
+			if (HMC.mode != HMC_OneShot) {
+				HMC.state = HMC_RX_ERR;
+				return;
+			}
+			break;
+		
+		default:
+			break;
+		}
+
+		enum HMC_ret ret;
+		ret = HMC_SendAddr_DMA();
+
+		if (ret == HMC_OK) {
+			HMC.state = HMC_ADDR_SENT;
+		} else {
+			HMC.state = HMC_RESET;
+		}
+	}
+}
+
+
+/*
+ *
+ * Condition measurements
+ *
+ */
+
+void HMC_ConditionMeasAll(void) {
+	/*
+	 *
+	 * Condition all measurement in HMC struct
+	 *
+	 */
+	HMC.measFloat.X = ((float)HMC.measurement.meas.X - HMC.Offs.xOffs) * HMC.sensitivity;
+	HMC.measFloat.Y = ((float)HMC.measurement.meas.Y - HMC.Offs.xOffs) * HMC.sensitivity;
+	HMC.measFloat.Z = (float)HMC.measurement.meas.Z * HMC.sensitivity;
+}
+
+
+float HMC_ConditionMeasSingle(int16_t meas_raw, float offset) {
+	/*
+	 *
+	 * Condition any individual measurement
+	 *
+	 */
+	return ((float)meas_raw - offset) * HMC.sensitivity;
+}
+
+
+/*
+ *
+ * Check timeout
+ *
+ */
+
+enum HMC_bool HMC_CheckTimeOut(void) {
+	/*
+	 *
+	 * Check if timeout has occurred
+	 *
+	 */
+
+	// calculate elapsed time
+	HMC.TimeElapsed.elapsed = ARM_CM_DWT_CYCCNT - HMC.TimeElapsed.currTicks;
+
+	// check whether the state has changed
+	if(HMC.state != HMC.TimeElapsed.prevState) {
+		HMC_ResetTimer();
+		return HMC_False;
+	}
+
+	// check whether timeout has occurred
+	if (HMC.TimeElapsed.elapsed > HMC.TimeElapsed.cycleTicks) {
+		return HMC_True;
+	} else {
+		return HMC_False;
+	}
+}
+
+
+void HMC_ResetTimer(void) {
+	/*
+	 *
+	 * Reset the timer
+	 *
+	 */
+
+	HMC.TimeElapsed.elapsed = 0;
+	HMC.TimeElapsed.currTicks = ARM_CM_DWT_CYCCNT;
+	HMC.TimeElapsed.prevState = HMC.state;
+}
+
+
+/*
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+}
+
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c){
+}
+
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c){
+}
+ */
--- HMC5883.h
+++ HMC5883.h
@@ -10,10 +10,10 @@
 
 
 /* ----------------
-*
-*	Register map - address defines
-*
- ----------------- */
+ *
+ *	Register map - address defines
+ *
+ * ----------------- */
  
 
 // Configuration
@@ -39,11 +39,11 @@
 #define HMC_ID_C										0x0C
 
 /* ----------------
-*
-*	Settings defines
-*
-*   Only non-default settings are defined
-*
+ *
+ *	Settings defines
+ *
+ *   Only non-default settings are defined
+ *
  ----------------- */
  
 
@@ -86,7 +86,6 @@
 #define HMC_CRB_GAIN5									0x05 << 5
 #define HMC_CRB_GAIN6									0x06 << 5
 #define HMC_CRB_GAIN7									0x07 << 5
-#define HMC_CRB_GAIN8									0x08 << 5
 
 
 // REG 2:	 		Mode Register
@@ -105,65 +104,143 @@
 #define HMC_ID_C_VAL									0x33
 
 
+// CORTEX-M - DWT TIMER
+#define  ARM_CM_DEMCR      								(*(uint32_t *)0xE000EDFC)
+#define  ARM_CM_DWT_CTRL   								(*(uint32_t *)0xE0001000)
+#define  ARM_CM_DWT_CYCCNT 								(*(uint32_t *)0xE0001004)
+
+
+
 /* ----------------
-*
-*
-*
- ----------------- */
+ *
+ *	Structures
+ *
+ * ----------------- */
+
+enum HMC_state{
+	HMC_INIT,
+	HMC_IDLE,
+	HMC_TRIGGERED,
+	HMC_ADDR_SENT,
+	HMC_DATA_RDY,
+	HMC_READING,
+	HMC_READ_DONE,
+	HMC_TIMEOUT,
+	HMC_RESET
+};
+
+
+enum HMC_ret{
+	HMC_OK,
+	HMC_ID_ERR,
+	HMC_TX_ERR,
+	HMC_TX_DMA_ERR,
+	HMC_RX_ERR,
+	HMC_RX_DMA_ERR,
+	HMC_ERR_TIMEOUT,
+	HMC_ERR_DMA_TIMEOUT
+};
+
+
+enum HMC_bool{
+	HMC_False,
+	HMC_True
+};
+
+
+enum HMC_mode{
+	HMC_Polling,
+	HMC_OneShot,
+	HMC_Cont
+};
+
+
+typedef union{
+	uint8_t raw[6];
+	struct{
+		int16_t X;
+		int16_t Y;
+		int16_t Z;
+	} meas;
+} meas_raw;
+
+
+typedef struct{
+	float X;
+	float Y;
+	float Z;
+} meas_f;
+
+
+typedef struct{
+	float xOffs;
+	float yOffs;
+} offset;
+
+
+typedef struct{
+	uint32_t currTicks;
+	uint32_t elapsed;
+	uint32_t systemClock;
+	enum HMC_state prevState;
+	uint32_t cycleTicks;
+} timer;
+
 
 typedef struct{
 		// Conditioning
 	uint8_t id[3];
 		// conditioned measurements
-	int16_t mag_X;
-	int16_t mag_Y;
-	int16_t mag_Z;
-		// float calc data
-	float mag_Xf;
-	float mag_Yf;
-	float mag_Zf;
+	meas_raw measurement;
+	meas_f measFloat;
+	float sensitivity;
+	offset Offs;
+		// Clocking, mode
+	timer TimeElapsed;
+	enum HMC_mode mode;
 		// Interface
 	I2C_HandleTypeDef *hi2c;
 	GPIO_TypeDef *int_PinPort;
 	uint16_t int_Pin;
-		// raw data
-	uint8_t raw[6];
 		// I2C reading
-	uint8_t reading_data;
-	uint8_t data_ready;
-	uint8_t meas_triggered;
+	enum HMC_state state;
 		// data regs
 	uint8_t reg_address[6];
 } HMC_data;
 
 
+/*
+ * Struct in use
+ */
+static HMC_data HMC;
+
+
 /* ----------------
-*
-*	Function prototypes
-*
- ----------------- */
+ *
+ *	Function prototypes
+ *
+ * ----------------- */
 
-uint8_t HMC_Init_Struct(HMC_data *input,
-		I2C_HandleTypeDef *hi2c,
-		GPIO_TypeDef *HMC_INT_Port, uint16_t HMC_IntPin);
-
-uint8_t HMC_Read_ID(HMC_data *input);
-
-uint8_t HMC_Config(HMC_data *input,
-						uint8_t average, uint8_t data_or, uint8_t meas_mode,
-						uint8_t gain, uint8_t mode);
-
-void HMC_Read_Rdy(HMC_data *input);
-
-uint8_t HMC_Trigger_Single_Meas(HMC_data *input);
-
-uint8_t HMC_Read_Poll(HMC_data *input);
-
-uint8_t HMC_SendAddr_DMA(HMC_data *input);
-
-uint8_t HMC_ReadMeas_DMA(HMC_data *input);
-
-void HMC_ReadCplt_DMA(HMC_data *input);
+enum HMC_ret HMC_Init_Struct(I2C_HandleTypeDef *hi2c, GPIO_TypeDef *HMC_INT_Port, uint16_t HMC_IntPin);
+enum HMC_ret HMC_Read_ID(void);
+void HMC_IntPinConfig(void);
+void HMC_SetSensitivity(uint8_t gain);
+enum HMC_ret HMC_Config_Cont(void);
+enum HMC_ret HMC_Config_Single(void);
+void HMC_Read_Rdy(void);
+void HMC_DmaTxCpltHandler(I2C_HandleTypeDef *hi2c);
+void HMC_DmaRxCpltHandler(I2C_HandleTypeDef *hi2c);
+void HMC_DataRdyPinHandler(void);
+enum HMC_ret HMC_Trigger_Single_Meas(void);
+enum HMC_ret HMC_Read_Poll(void);
+enum HMC_ret HMC_SendAddr_DMA(void);
+enum HMC_ret HMC_ReadMeas_DMA(void);
+void HMC_ReadCplt_DMA(void);
+void HMC_StateMachine(void);
+void HMC_Int(void);
+enum HMC_bool HMC_CheckTimeOut(void);
+void HMC_ResetTimer(void);
+void HMC_SetCycleTicks(uint8_t meas_mode, uint8_t op_mode, uint8_t data_or);
 
 
 #endif
